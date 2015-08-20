29eee2b47401a4581c91361cb467b02d47c17c72
Brandon Swanson <swanyriver@gmail.com>
2015-08-19 22:27:59 -0700

    working palindrome

--- palindrome.c
+++ palindrome.c
@@ -13,7 +13,7 @@ char* longestPalindrome(char* s) {
         
         left = middle;
         right = middle;
-        while(left>s && *right){
+        while(left>=s && *right){
             if(*left == *right){
                 if (right-left >= longest ) {
                     longest = (right-left)+1;
@@ -28,15 +28,41 @@ char* longestPalindrome(char* s) {
         
         ++middle;
     }
+
+    //even numbered palindromes
+    middle=s;
+    while(*(middle+1)){
+
+        left=middle;
+        right=middle+1;
+
+        while(left>=s && *right){
+            if(*left == *right){
+                if (right-left >= longest ) {
+                    longest = (right-left)+1;
+                    begining = left;
+                }
+                
+                --left;
+                ++right;
+            } 
+            else break;
+        }
+
+        ++middle;
+    }
     
     *(begining+longest)=0; 
     return begining;
 }
 
-int main(){
+int main(int argc, char const *argv[])
+{
 
-    char* s = malloc(9);
-    strcpy(s,"abcdedcz");
+    if(argc<2) return 1;
+
+    char* s = malloc(40);
+    strcpy(s,argv[1]);
 
     printf("%s\n", longestPalindrome(s));
 
