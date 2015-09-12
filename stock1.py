75bc465e5bc6e3a6e61803c39c67186d7d7c4611
Brandon Swanson <swanyriver@gmail.com>
2015-09-11 18:06:20 -0700

    working chain but only if objects passed in have dict

--- stock1.py
+++ stock1.py
@@ -1,15 +1,59 @@
-import heapq
 class local(object):
-    """docstring for ClassName"""
-    def __init__(self, Min, Max, prev):
+    """A connection from local minimum to local maximum"""
+    def __init__(self, Min, Max):
         #super(local, self).__init__()
         self.min = Min
         self.max = Max
-        self.prev = prev
-        self.next = None
     def __repr__(self):
-        return "(%d,%d) id:%r p:%r, n:%r"%(self.min,self.max, self.ID, self.prev, self.next)
+        return "(%d,%d)"%(self.min,self.max)
+    def __str__(self): return self.__repr__()
     def diff(self): return self.max - self.min
+
+class Chain(object):
+    """doubly linked list of locals"""
+    def __init__(self):
+        self.begining = None
+        self.end = None
+        self.count = 0
+    def __len__(self):
+        return self.count
+
+    def add(self,s):
+        s.prev = self.end
+        s.next = None
+        self.end = s
+        self.count +=1
+        if not self.begining:
+            self.begining = s
+            self.end = s
+        else:
+            s.prev.next = s
+
+    def remove(self,s):
+        self.count -=1
+        if not self.count:
+            self.begining = None
+            self.end = None
+        elif s is self.begining:
+            self.begining = s.next
+            self.begining.prev = None
+        elif s is self.end:
+            self.end = s.prev
+            self.end.next = None
+        else:
+            s.prev.next = s.next
+            s.next.prev = s.prev
+
+    def __iter__(self):
+        snext = self.begining
+        while snext:
+            yield snext
+            snext = snext.next
+
+    def __repr__(self):
+        return "->".join(map(str,self))
+    def __str__(self): return self.__repr__()
+
 
         
 
@@ -72,14 +116,6 @@ class Solution(object):
         print slopes
         print "------------------------------"
 
-        # import matplotlib.pyplot as plt
-        # plt.plot(prices, 'ro')
-        # #for loc in slopes: plt.plot([loc.min,loc.max])
-        # plt.ylabel('price')
-        # plt.show()
-
-        # def elimc(s1,s2):
-        #     return (s1.diff() + s2.diff()) - (s2.max - s1.min)
         def elimc(s):
             if s.next and slopes[s.next].max > s.max: 
                 s.merge = True
@@ -92,20 +128,33 @@ class Solution(object):
 
         for s in slopes.values():
             pque.append( (elimc(s),s) )
-        # for i in xrange(len(slopes)):
-        #     if i<len(slopes)-1 and slopes[i+1].max > slopes[i].max:
-        #         #calculate merge cost
-        #         slopes[i].elimCost = elimc(slopes[i],slopes[i+1])
-        #     else:
-        #         slopes[i].elimCost = slopes[i].diff()
-        #     #pque.append(slopes[i])
-            ##need to make a tuple 
 
         heapq.heapify(pque)
+
         for s in pque:
             print s
 
-        #while len(slopes) > k:
+        while len(slopes) > k:
+            pass
+            #get the next lowest
+
+            #heap pop (while id not in slopes dict)
+
+            #NOT MERGE
+            #stich prev and next
+            #remove from slopes dict
+
+            #MERGE
+            #create new item and remove both merged list from dict
+            #cache prev and next objects from
+            #add item to dictionary
+            #set new items prev, next
+            #set neighbors to point at new index
+            #recaculate elimcost for neighbors
+
+
+
+        #sum up diffs
             
 
 
@@ -114,4 +163,8 @@ x = Solution()
 # x.maxProfit([1,3,5,2,8,9,6])
 # x.maxProfit([1,3,3,3,4,4,5,2,8,9,6])
 # x.maxProfit([5,3,5,6,8,8,2,8,9,6])
-x.maxProfit([5,3,5,6,8,8,2,8,9,6,10,1,3,4,0,3,1,9])
+#x.maxProfit([5,3,5,6,8,8,2,8,9,6,10,1,3,4,0,3,1,9])
+
+c = Chain()
+for x in range(10): c.add( local(x,x+3))
+
